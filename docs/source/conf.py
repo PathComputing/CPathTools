6bbc80f58c3a26ddf3260724ab7278bdc7dfb498
PingjunChen <pingjunchen@ieee.org>
2022-01-24 12:16:19 -0600

    update configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,10 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os, sys
+sys.path.insert(0, os.path.abspath('.'))
+from sphinx.application import Sphinx
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -27,8 +31,12 @@ author = 'Pingjun Chen'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ["sphinx.ext.mathjax",
+        "sphinx.ext.ifconfig",
+        "sphinx.ext.viewcode",
 ]
+
+language = 'English'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +52,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+pygments_style = 'sphinx'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
